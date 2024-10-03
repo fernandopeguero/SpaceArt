386d1850909b8cf61f8c5b0e9232718b7785b0b5
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-10-03 15:17:47 -0400

    Update style center text and change button font size

--- app/src/main/java/com/example/spaceart/MainActivity.kt
+++ app/src/main/java/com/example/spaceart/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -12,7 +13,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -26,6 +30,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -94,12 +99,17 @@ fun SpaceArtApp(modifier: Modifier = Modifier) {
                 fontSize = 24.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color.White,
-                modifier = Modifier.padding(start = 8.dp)
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .padding(start = 8.dp, bottom = 16.dp)
+                    .fillMaxWidth()
             )
 
             Row(
                 horizontalArrangement = Arrangement.SpaceBetween,
-                modifier = Modifier.fillMaxWidth().padding(8.dp)
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
             ) {
                 Button(
                     onClick = {
@@ -109,9 +119,14 @@ fun SpaceArtApp(modifier: Modifier = Modifier) {
                         } else {
                             currentPosition--
                         }
-                    }
+                    },
+                    shape = RoundedCornerShape(5.dp)
                 ) {
-                    Text(text = stringResource(R.string.previous_button))
+                    Text(
+                        text = stringResource(R.string.previous_button),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
+                    )
                 }
 
                 Button(
@@ -121,10 +136,13 @@ fun SpaceArtApp(modifier: Modifier = Modifier) {
                         } else {
                             currentPosition++
                         }
-                    }
+                    },
+                    shape = RoundedCornerShape(5.dp)
                 ) {
                     Text(
-                        text = stringResource(R.string.next_button)
+                        text = stringResource(R.string.next_button),
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 18.sp
                     )
                 }
             }
@@ -139,7 +157,7 @@ fun SpaceArtApp(modifier: Modifier = Modifier) {
 fun PreviewSpaceArt() {
 
     SpaceArtTheme {
-
+        SpaceArtApp()
     }
 }
 
